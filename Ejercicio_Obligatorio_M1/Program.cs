24dbac6ce704e5f0a707bb4c60430e30e0956eab
matias9486 <matias9486mdp@gmail.com>
2024-07-23 18:50:17 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio_Obligatorio_M1/Program.cs
@@ -0,0 +1,150 @@
+﻿/* Crear una aplicación simple de consola para el siguiente escenario:
+Escenario: Weather Forecast
+
+1. Solicitar al usuario que introduzca la temperatura actual (en grados Celsius).
+2. Basándose en la temperatura introducida, la aplicación debería mostrar un mensaje:
+   - Si la temperatura es inferior a 0, mostrar "Hace mucho frío afuera, asegúrate de abrigarte bien."
+   - Si la temperatura está entre 0 y 20, mostrar "El clima está fresco, una chaqueta ligera sería perfecta."
+   - Si la temperatura es superior a 20, mostrar "Hace calor afuera, no necesitas una chaqueta."
+3. Luego, la aplicación debería preguntar al usuario si quiere conocer el pronóstico para los próximos cinco días (sí/no).
+4. Si el usuario responde 'sí', la aplicación debería generar y mostrar un pronóstico simple. 
+5. Finalmente, la aplicación debería preguntar al usuario si quiere consultar otro pronóstico. 
+   - Si el usuario responde 'sí', la aplicación debería comenzar de nuevo. 
+   - Si el usuario responde 'no', la aplicación debería mostrar un mensaje de despedida y terminar.
+ */
+
+bool pronostico = true;
+bool pronosticoExtendido = true;
+bool pronosticoOtro = true;
+
+string mensaje;
+double temperatura;
+int opcionElegida;
+
+int min = -5; //rango de temperatura mínimo
+int max = 30;  //rango de temperatura máximo
+
+// Crear una instancia de Random para generar números aleatorios
+Random random = new Random();
+int temperaturaRandom;
+
+do
+{
+    Console.WriteLine("Ingrese la temperatura actual (En grados Celsius)");
+    if (!double.TryParse(Console.ReadLine(), out temperatura))
+    {
+        Console.WriteLine("Ingrese una opción válida. Presione enter para continuar.");
+        Console.ReadLine();
+        Console.Clear(); // Limpiar la consola para comenzar de nuevo
+        continue;
+    }
+    else
+    {
+        switch (temperatura)
+        {
+            case double t when t < 0:
+                mensaje = "Hace mucho frío afuera, asegúrate de abrigarte bien.";
+                break;
+            case double t when t >= 0 && t <= 20:
+                mensaje = "El clima está fresco, una chaqueta ligera sería perfecta.";
+                break;
+            case var t when t > 20:
+                mensaje = "Hace calor afuera, no necesitas una chaqueta.";
+                break;
+            default:
+                mensaje = "No se puede determinar el clima.";
+                break;
+        }
+
+        Console.WriteLine(mensaje);
+    }
+
+    //pronóstico extendido
+    do
+    {        
+        Console.WriteLine("\n¿Quieres conocer el pronóstico para los próximos cinco días? 1-Sí. 2-No:");
+        if (!int.TryParse(Console.ReadLine(), out opcionElegida))
+        {
+            Console.WriteLine("Ingrese una opción válida. Presione enter para continuar.");
+            Console.ReadLine();
+            Console.Clear();
+            continue;
+        }
+        else
+        {
+            if (opcionElegida == 1)
+            {
+                Console.WriteLine("\nPronóstico para los próximos cinco días:");
+                // Se genera un número double aleatorio entre min (inclusive) y max (exclusive)
+                temperaturaRandom = (int)(min + (random.NextDouble() * (max - min)));
+                // Interpolación de cadenas
+                Console.WriteLine($"Lunes: {temperaturaRandom} ºC");
+
+                temperaturaRandom = (int)(min + (random.NextDouble() * (max - min)));
+                Console.WriteLine($"Martes: {temperaturaRandom} ºC");
+
+                temperaturaRandom = (int)(min + (random.NextDouble() * (max - min)));
+                Console.WriteLine($"Miércoles: {temperaturaRandom} ºC");
+
+                temperaturaRandom = (int)(min + (random.NextDouble() * (max - min)));
+                Console.WriteLine($"Jueves: {temperaturaRandom} ºC");
+
+                temperaturaRandom = (int)(min + (random.NextDouble() * (max - min)));
+                Console.WriteLine($"Viernes: {temperaturaRandom} ºC");
+
+                pronosticoExtendido = false;
+            }
+            else
+            {
+                if(opcionElegida == 2){
+                    pronosticoExtendido = false;
+                }
+                else
+                {
+                    if (opcionElegida != 2){
+                        Console.WriteLine("Ingrese una opción válida. Presione enter para continuar.");
+                        Console.ReadLine();
+                        Console.Clear();
+                        continue;
+                    }
+                }                
+            }
+        }
+    } while (pronosticoExtendido);
+
+    //consultar otro prónostico
+    do
+    {
+        Console.WriteLine("\n¿Quieres consultar otro pronóstico? 1-Sí. 2-No:");
+        if (!int.TryParse(Console.ReadLine(), out opcionElegida)){
+            Console.WriteLine("Ingrese una opción válida. Presione enter para continuar.");
+            Console.ReadLine();
+            Console.Clear();
+            continue;
+        }
+        else
+        {            
+            if(opcionElegida == 1){
+                pronostico = true;
+                pronosticoOtro = false;
+                Console.Clear();
+            }
+            else{
+                if (opcionElegida == 2){
+                    Console.WriteLine("\nGracias por usar nuestra aplicación de pronóstico del clima. ¡Hasta luego!");
+                    pronostico = false;
+                    pronosticoOtro = false;
+                }
+                else{
+                    if (opcionElegida != 1 && opcionElegida != 2){
+                        Console.WriteLine("Ingrese una opción válida. Presione enter para continuar.");
+                        Console.ReadLine();
+                        Console.Clear();
+                        pronosticoOtro = true;
+                    }                                 
+                }
+            }
+        }                                    
+    } while (pronosticoOtro);        
+
+} while (pronostico);
---
+++ Ejercicios_Opcionales_M1/Program.cs
@@ -0,0 +1,158 @@
+﻿int opcion;
+int valor;
+int resultado;
+string mensaje = "";
+
+do
+{
+    Console.WriteLine("1. Dado un valor, devolver un mensaje que diga “El valor es mayor que 100” sólo cuando se cumpla dicha condición.");
+    Console.WriteLine("2. Pedir un número entero por teclado y calcular si es par o impar.");
+    Console.WriteLine("3. Teniendo un valor entero, verificar si se cumple o no que ese valor es el doble de un impar. Por ejemplo, 14 cumple con esta condición.");
+    Console.WriteLine("4. Dada un número del 1 al 10, devolver su “versión” en números romanos.");
+    Console.WriteLine("5. Salir");
+
+    Console.WriteLine("\nElija la opción a realizar: ");
+    if (!int.TryParse(Console.ReadLine(), out opcion))
+    {
+        Console.WriteLine("Ingrese una opción válida. Presione enter para continuar.");
+        Console.ReadLine();
+        Console.Clear(); // Limpiar la consola para comenzar de nuevo
+        continue;
+    }
+
+    switch (opcion)
+    {
+        case 1:
+            Console.WriteLine("Ingrese un número entero:");
+            if (!int.TryParse(Console.ReadLine(), out valor))
+            {
+                Console.WriteLine("\nNo ingresó una opción válida. Presione enter para continuar.");
+                Console.ReadLine();
+                Console.Clear(); // Limpiar la consola para comenzar de nuevo                
+            }
+            else
+            {
+                if (valor > 100)
+                {
+                    Console.WriteLine("\nEl valor es mayor que 100. Presione enter para continuar.");
+                    Console.ReadLine();
+                    Console.Clear(); // Limpiar la consola para comenzar de nuevo 
+                }                    
+            }
+            break;
+        case 2:
+            Console.WriteLine("\nIngrese un número entero:");
+            if (!int.TryParse(Console.ReadLine(), out valor))
+            {
+                Console.WriteLine("\nNo ingresó una opción válida. Presione enter para continuar.");
+                Console.ReadLine();
+                Console.Clear(); // Limpiar la consola para comenzar de nuevo                
+            }
+            else
+            {
+                if (valor % 2 == 0)
+                    Console.Write($"\n{valor} es par.");
+                else
+                    Console.Write($"\n{valor} es impar.");
+
+                Console.WriteLine("Presione enter para continuar.");
+                Console.ReadLine();
+                Console.Clear();
+            }
+            
+            break;
+        case 3:
+            Console.WriteLine("\nIngrese un número entero:");
+            if (!int.TryParse(Console.ReadLine(), out valor))
+            {
+                Console.WriteLine("\nNo ingresó una opción válida. Presione enter para continuar.");
+                Console.ReadLine();
+                Console.Clear(); // Limpiar la consola para comenzar de nuevo
+                continue;
+            }
+            else
+            {
+                resultado = valor / 2;
+                if (resultado % 2 != 0)
+                {
+                    Console.WriteLine($"\n{valor} es el doble de {resultado}. Presione enter para continuar.");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+            }
+            break;
+        case 4:
+            Console.WriteLine("\nIngrese un número entero del 1 al 10:");
+            if (!int.TryParse(Console.ReadLine(), out valor))
+            {
+                Console.WriteLine("\nNo ingresó una opción válida. Presione enter para continuar.");
+                Console.ReadLine();
+                Console.Clear(); // Limpiar la consola para comenzar de nuevo
+                continue;
+            }
+            else
+            {
+               if(valor>=1 && valor <= 10)
+                {
+                    switch (valor)
+                    {
+                        case 1:
+                            mensaje = $"{valor} en números romanos es I";
+                            break;
+                        case 2:
+                            mensaje = $"{valor} en números romanos es II";
+                            break;
+                        case 3:
+                            mensaje = $"{valor} en números romanos es III";
+                            break;
+                        case 4:
+                            mensaje = $"{valor} en números romanos es IV";
+                            break;
+                        case 5:
+                            mensaje = $"{valor} en números romanos es V";
+                            break;
+                        case 6:
+                            mensaje = $"{valor} en números romanos es VI";
+                            break;
+                        case 7:
+                            mensaje = $"{valor} en números romanos es VII";
+                            break;
+                        case 8:
+                            mensaje = $"{valor} en números romanos es VIII";
+                            break;
+                        case 9:
+                            mensaje = $"{valor} en números romanos es IX";
+                            break;
+                        case 10:
+                            mensaje = $"{valor} en números romanos es X";
+                            break;                        
+                        default:
+                            Console.WriteLine("No ingresó un número válido.");
+                            break;
+                    }
+                    Console.WriteLine($"\n{mensaje}. Presione enter para continuar.");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+                else
+                {
+                    Console.WriteLine("\nNo ingresó un número válido. Presione enter para continuar.");
+                    Console.ReadLine();
+                    Console.Clear(); // Limpiar la consola para comenzar de nuevo
+                    continue;
+                }
+            }
+            break;
+
+        //SALIR
+        case 5:
+            Console.WriteLine("\nGracias por usar la aplicación.");
+            break;
+        default:
+            Console.WriteLine("\nNo ingresó una opción válida. Presione enter para continuar.");
+            Console.ReadLine();
+            Console.Clear(); // Limpiar la consola para comenzar de nuevo 
+            break;
+    }
+
+} while (opcion!=5);
